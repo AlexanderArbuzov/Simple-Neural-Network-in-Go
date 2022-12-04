bd4d02a1bc4e277d757cb4498a4433b8c23cd3ee
AlexanderArbuzov <89941850+AlexanderArbuzov@users.noreply.github.com>
2022-12-04 12:46:36 +0300

    Update Simple-Neural-Network-in-Go.go

--- Simple-Neural-Network-in-Go.go
+++ Simple-Neural-Network-in-Go.go
@@ -8,84 +8,82 @@ import (
 func main() {
 
 	var (
-		numberOfInputVal int
-		iterations       int
-		outputLog        int
-		eta              float64
+		NUMBEROFINPUTVAL = 3
+		ITERATIONS       = 10001
+		OUTPUTLOG        = 1000
+		ETA              = 0.01
+		X                = []float64{0.03, 0.72, 0.49}
+		Y                = []float64{0.93, 0.74, 0.17}
+		weights1         = [][]float64{{0.88, 0.39, 0.9}, {0.37, 0.14, 0.41}, {0.96, 0.5, 0.6}}
+		biases2          = []float64{0.23, 0.89, 0.08}
+		weights2         = [][]float64{{0.29, 0.57, 0.36}, {0.73, 0.53, 0.68}, {0.01, 0.02, 0.58}}
+		biases3          = []float64{0.78, 0.83, 0.80}
 		meanSquaredError float64
-		Input            []float64
-		W1               [][]float64
-		B2               []float64
-		W2               [][]float64
-		B3               []float64
-		Y                []float64
 	)
 
-	Input, B2, B3, Y, W1, W2 = inputData(&numberOfInputVal, &iterations, &outputLog, &eta, &Input, &B2, &B3, &Y, &W1, &W2)
-
-	for l := 0; l < iterations; l++ {
+	for l := 0; l < ITERATIONS; l++ {
 
 		// Forward Propagation
 
-		Hidden := HiddenLayer(numberOfInputVal, &Input, &B2, &W1)
+		hidden := hiddenLayer(NUMBEROFINPUTVAL, &X, &biases2, &weights1)
 
-		Output := OutputLayer(numberOfInputVal, Hidden, &B3, &W2)
+		output := outputLayer(NUMBEROFINPUTVAL, hidden, &biases3, &weights2)
 
 		// Backward Propagation
 
-		// W2
+		// weights2
 
-		d := d(numberOfInputVal, Output, &Y)
+		d := d(NUMBEROFINPUTVAL, output, &Y)
 
-		dW2 := dW2(numberOfInputVal, d, Hidden)
+		dW2 := dW2(NUMBEROFINPUTVAL, d, hidden)
 
-		// B3
+		// biases3
 
-		dB3 := dB3(numberOfInputVal, d)
+		dB3 := dB3(NUMBEROFINPUTVAL, d)
 
-		// W1
+		// weights1
 
-		dW2A := dW2A(numberOfInputVal, d, &W2)
+		dW2A := dW2A(NUMBEROFINPUTVAL, d, &weights2)
 
-		dW1 := dW1(numberOfInputVal, dW2A, &Input, Hidden)
+		dW1 := dW1(NUMBEROFINPUTVAL, dW2A, &X, hidden)
 
-		// B2
+		// biases2
 
-		dB2 := dB2(numberOfInputVal, dW2A, Hidden)
+		dB2 := dB2(NUMBEROFINPUTVAL, dW2A, hidden)
 
 		// Output Log
 
 		meanSquaredError = 0
 
-		if l != 0 && l%outputLog == 0 {
+		if l != 0 && l%OUTPUTLOG == 0 {
 			Println()
 
 			Println("Epoch: ", l)
 
-			for i := 0; i < numberOfInputVal; i++ {
-				meanSquaredError += Pow(Y[i]-(*Output)[i], 2)
+			for i := 0; i < NUMBEROFINPUTVAL; i++ {
+				meanSquaredError += Pow(Y[i]-(*output)[i], 2)
 			}
 
-			Printf("Error: %.9f\n", meanSquaredError/float64(numberOfInputVal))
+			Printf("Error: %.9f\n", meanSquaredError/float64(NUMBEROFINPUTVAL))
 
 			Println()
 
-			Println("=================== Weights W1 ===================")
+			Println("=================== Weights 1 ===================")
 
-			for i := 0; i < numberOfInputVal; i++ {
-				for j := 0; j < numberOfInputVal; j++ {
-					Printf("%.9f ", W1[i][j])
+			for i := 0; i < NUMBEROFINPUTVAL; i++ {
+				for j := 0; j < NUMBEROFINPUTVAL; j++ {
+					Printf("%.9f ", weights1[i][j])
 				}
 				Println()
 			}
 
 			Println()
 
-			Println("=================== Weights W2 ===================")
+			Println("=================== Weights 2 ===================")
 
-			for i := 0; i < numberOfInputVal; i++ {
-				for j := 0; j < numberOfInputVal; j++ {
-					Printf("%.9f ", W2[i][j])
+			for i := 0; i < NUMBEROFINPUTVAL; i++ {
+				for j := 0; j < NUMBEROFINPUTVAL; j++ {
+					Printf("%.9f ", weights2[i][j])
 				}
 				Println()
 			}
@@ -94,8 +92,8 @@ func main() {
 
 			Println("===================   Output   ===================")
 
-			for i := 0; i < numberOfInputVal; i++ {
-				Printf("%.9f ", (*Output)[i])
+			for i := 0; i < NUMBEROFINPUTVAL; i++ {
+				Printf("%.9f ", (*output)[i])
 			}
 
 			Println()
@@ -103,288 +101,132 @@ func main() {
 
 		// Updating Weights
 
-		for i := 0; i < numberOfInputVal; i++ {
-			for j := 0; j < numberOfInputVal; j++ {
-				W2[i][j] += eta * (*dW2)[i][j]
+		for i := 0; i < NUMBEROFINPUTVAL; i++ {
+			for j := 0; j < NUMBEROFINPUTVAL; j++ {
+				weights2[i][j] += ETA * (*dW2)[i][j]
 			}
 		}
 
-		for i := 0; i < numberOfInputVal; i++ {
-			B3[i] += eta * (*dB3)[i]
+		for i := 0; i < NUMBEROFINPUTVAL; i++ {
+			biases3[i] += ETA * (*dB3)[i]
 		}
 
-		for i := 0; i < numberOfInputVal; i++ {
-			for j := 0; j < numberOfInputVal; j++ {
-				W1[i][j] += eta * (*dW1)[i][j]
+		for i := 0; i < NUMBEROFINPUTVAL; i++ {
+			for j := 0; j < NUMBEROFINPUTVAL; j++ {
+				weights1[i][j] += ETA * (*dW1)[i][j]
 			}
 		}
 
-		for i := 0; i < numberOfInputVal; i++ {
-			B2[i] += eta * (*dB2)[i]
+		for i := 0; i < NUMBEROFINPUTVAL; i++ {
+			biases2[i] += ETA * (*dB2)[i]
 		}
 	}
 }
 
-func HiddenLayer(numberOfInputVal int, Input, B2 *[]float64, W1 *[][]float64) *[]float64 {
+func hiddenLayer(NUMBEROFINPUTVAL int, X, biases2 *[]float64, weights1 *[][]float64) *[]float64 {
 
-	var Hidden = make([]float64, numberOfInputVal, numberOfInputVal)
+	var hidden = make([]float64, NUMBEROFINPUTVAL, NUMBEROFINPUTVAL)
 
-	for i := 0; i < numberOfInputVal; i++ {
-		for j := 0; j < numberOfInputVal; j++ {
-			Hidden[i] += (*W1)[i][j] * (*Input)[j]
+	for i := 0; i < NUMBEROFINPUTVAL; i++ {
+		for j := 0; j < NUMBEROFINPUTVAL; j++ {
+			hidden[i] += (*weights1)[i][j] * (*X)[j]
 		}
-		Hidden[i] = 1 / (1 + (Exp(-Hidden[i] - (*B2)[i])))
+		hidden[i] = 1 / (1 + (Exp(-hidden[i] - (*biases2)[i])))
 	}
 
-	return &Hidden
+	return &hidden
 }
 
-func OutputLayer(numberOfInputVal int, Hidden, B3 *[]float64, W2 *[][]float64) *[]float64 {
+func outputLayer(NUMBEROFINPUTVAL int, hidden, biases3 *[]float64, weights2 *[][]float64) *[]float64 {
 
-	var Output = make([]float64, numberOfInputVal, numberOfInputVal)
+	var output = make([]float64, NUMBEROFINPUTVAL, NUMBEROFINPUTVAL)
 
-	for i := 0; i < numberOfInputVal; i++ {
-		for j := 0; j < numberOfInputVal; j++ {
-			Output[i] += (*W2)[i][j] * (*Hidden)[j]
+	for i := 0; i < NUMBEROFINPUTVAL; i++ {
+		for j := 0; j < NUMBEROFINPUTVAL; j++ {
+			output[i] += (*weights2)[i][j] * (*hidden)[j]
 		}
-		Output[i] = 1 / (1 + (Exp(-Output[i] - (*B3)[i])))
+		output[i] = 1 / (1 + (Exp(-output[i] - (*biases3)[i])))
 	}
 
-	return &Output
+	return &output
 }
 
-func d(numberOfInputVal int, Output, Y *[]float64) *[]float64 {
+func d(NUMBEROFINPUTVAL int, output, Y *[]float64) *[]float64 {
 
-	var d = make([]float64, numberOfInputVal, numberOfInputVal)
+	var d = make([]float64, NUMBEROFINPUTVAL, NUMBEROFINPUTVAL)
 
-	for i := 0; i < numberOfInputVal; i++ {
-		d[i] = ((*Y)[i] - (*Output)[i]) * (*Output)[i] * (1 - (*Output)[i])
+	for i := 0; i < NUMBEROFINPUTVAL; i++ {
+		d[i] = ((*Y)[i] - (*output)[i]) * (*output)[i] * (1 - (*output)[i])
 	}
 
 	return &d
 }
 
-func dW2(numberOfInputVal int, d, Hidden *[]float64) *[][]float64 {
+func dW2(NUMBEROFINPUTVAL int, d, hidden *[]float64) *[][]float64 {
 
-	var dW2 = make([][]float64, numberOfInputVal, numberOfInputVal)
+	var dW2 = make([][]float64, NUMBEROFINPUTVAL, NUMBEROFINPUTVAL)
 
-	for i := 0; i < numberOfInputVal; i++ {
-		dW2[i] = make([]float64, numberOfInputVal)
+	for i := 0; i < NUMBEROFINPUTVAL; i++ {
+		dW2[i] = make([]float64, NUMBEROFINPUTVAL)
 	}
 
-	for i := 0; i < numberOfInputVal; i++ {
-		for j := 0; j < numberOfInputVal; j++ {
-			dW2[i][j] = 2 * ((*d)[i] * (*Hidden)[j]) / float64(numberOfInputVal)
+	for i := 0; i < NUMBEROFINPUTVAL; i++ {
+		for j := 0; j < NUMBEROFINPUTVAL; j++ {
+			dW2[i][j] = 2 * ((*d)[i] * (*hidden)[j]) / float64(NUMBEROFINPUTVAL)
 		}
 	}
 
 	return &dW2
 }
 
-func dB3(numberOfInputVal int, d *[]float64) *[]float64 {
+func dB3(NUMBEROFINPUTVAL int, d *[]float64) *[]float64 {
 
-	var dB3 = make([]float64, numberOfInputVal, numberOfInputVal)
+	var dB3 = make([]float64, NUMBEROFINPUTVAL, NUMBEROFINPUTVAL)
 
-	for i := 0; i < numberOfInputVal; i++ {
-		dB3[i] = 2 * (*d)[i] / float64(numberOfInputVal)
+	for i := 0; i < NUMBEROFINPUTVAL; i++ {
+		dB3[i] = 2 * (*d)[i] / float64(NUMBEROFINPUTVAL)
 	}
 
 	return &dB3
 }
 
-func dW2A(numberOfInputVal int, d *[]float64, W2 *[][]float64) *[]float64 {
+func dW2A(NUMBEROFINPUTVAL int, d *[]float64, weights2 *[][]float64) *[]float64 {
 
-	var dW2A = make([]float64, numberOfInputVal, numberOfInputVal)
+	var dW2A = make([]float64, NUMBEROFINPUTVAL, NUMBEROFINPUTVAL)
 
-	for i := 0; i < numberOfInputVal; i++ {
-		for j := 0; j < numberOfInputVal; j++ {
-			dW2A[i] += (*d)[j] * (*W2)[j][i]
+	for i := 0; i < NUMBEROFINPUTVAL; i++ {
+		for j := 0; j < NUMBEROFINPUTVAL; j++ {
+			dW2A[i] += (*d)[j] * (*weights2)[j][i]
 		}
 	}
 
 	return &dW2A
 }
 
-func dW1(numberOfInputVal int, dW2A, Input, Hidden *[]float64) *[][]float64 {
+func dW1(NUMBEROFINPUTVAL int, dW2A, X, hidden *[]float64) *[][]float64 {
 
-	var dW1 = make([][]float64, numberOfInputVal, numberOfInputVal)
+	var dW1 = make([][]float64, NUMBEROFINPUTVAL, NUMBEROFINPUTVAL)
 
-	for i := 0; i < numberOfInputVal; i++ {
-		dW1[i] = make([]float64, numberOfInputVal)
+	for i := 0; i < NUMBEROFINPUTVAL; i++ {
+		dW1[i] = make([]float64, NUMBEROFINPUTVAL)
 	}
 
-	for i := 0; i < numberOfInputVal; i++ {
-		for j := 0; j < numberOfInputVal; j++ {
-			dW1[i][j] = 2 * ((*dW2A)[i] * (*Hidden)[i] * (1 - (*Hidden)[i]) * (*Input)[j]) / float64(numberOfInputVal)
+	for i := 0; i < NUMBEROFINPUTVAL; i++ {
+		for j := 0; j < NUMBEROFINPUTVAL; j++ {
+			dW1[i][j] = 2 * ((*dW2A)[i] * (*hidden)[i] * (1 - (*hidden)[i]) * (*X)[j]) / float64(NUMBEROFINPUTVAL)
 		}
 	}
 
 	return &dW1
 }
 
-func dB2(numberOfInputVal int, dW2A, Hidden *[]float64) *[]float64 {
+func dB2(NUMBEROFINPUTVAL int, dW2A, hidden *[]float64) *[]float64 {
 
-	var dB2 = make([]float64, numberOfInputVal, numberOfInputVal)
+	var dB2 = make([]float64, NUMBEROFINPUTVAL, NUMBEROFINPUTVAL)
 
-	for i := 0; i < numberOfInputVal; i++ {
-		dB2[i] = 2 * ((*dW2A)[i] * (*Hidden)[i] * (1 - (*Hidden)[i]) * 1) / float64(numberOfInputVal)
+	for i := 0; i < NUMBEROFINPUTVAL; i++ {
+		dB2[i] = 2 * ((*dW2A)[i] * (*hidden)[i] * (1 - (*hidden)[i]) * 1) / float64(NUMBEROFINPUTVAL)
 	}
 
 	return &dB2
-}
-
-func inputData(numberOfInputVal, iterations, outputLog *int, eta *float64, Input, B2, B3, Y *[]float64, W1, W2 *[][]float64) ([]float64, []float64, []float64, []float64, [][]float64, [][]float64) {
-
-	var (
-		defData string
-		err     error
-	)
-
-	for ok := true; ok; ok = defData != "Y" && defData != "y" && defData != "N" && defData != "n" {
-		Print("Use default data (Y/N) ")
-		_, err = Scan(&defData)
-		if err != nil {
-			Println(err)
-		}
-	}
-
-	if defData == "Y" || defData == "y" {
-
-		// Default data
-
-		*numberOfInputVal = 3
-		*eta = 0.01
-		*iterations = 10001
-		*outputLog = 1000
-		Input = &[]float64{0.03, 0.72, 0.49}
-		W1 = &[][]float64{{0.88, 0.39, 0.9}, {0.37, 0.14, 0.41}, {0.96, 0.5, 0.6}}
-		B2 = &[]float64{0.23, 0.89, 0.08}
-		W2 = &[][]float64{{0.29, 0.57, 0.36}, {0.73, 0.53, 0.68}, {0.01, 0.02, 0.58}}
-		B3 = &[]float64{0.78, 0.83, 0.80}
-		Y = &[]float64{0.93, 0.74, 0.17}
-
-	} else if defData == "N" || defData == "n" {
-
-		for ok := true; ok; ok = err != nil {
-			Print("Number of input values? ")
-			_, err = Scan(&*numberOfInputVal)
-			if err != nil {
-				Println(err)
-			}
-		}
-
-		for ok := true; ok; ok = err != nil {
-			Print("Learning rate? ")
-			_, err = Scan(&*eta)
-			if err != nil {
-				Println(err)
-			}
-		}
-
-		for ok := true; ok; ok = err != nil {
-			Print("Number of iterations? ")
-			_, err = Scan(&*iterations)
-			if err != nil {
-				Println(err)
-			}
-		}
-
-		for ok := true; ok; ok = err != nil {
-			Print("Value 'outputLog'? (Depends on the number of iterations.) ")
-			// Example. If "iterations" = 101 and "outputLog" = 10: iterations / outputLog = 10 log messages.
-			_, err = Scan(&*outputLog)
-			if err != nil {
-				Println(err)
-			}
-		}
-
-		Println()
-
-		*Input = make([]float64, *numberOfInputVal, *numberOfInputVal)
-		*W1 = make([][]float64, *numberOfInputVal, *numberOfInputVal)
-		*B2 = make([]float64, *numberOfInputVal, *numberOfInputVal)
-		*W2 = make([][]float64, *numberOfInputVal, *numberOfInputVal)
-		*B3 = make([]float64, *numberOfInputVal, *numberOfInputVal)
-		*Y = make([]float64, *numberOfInputVal, *numberOfInputVal)
-
-		for i := 0; i < *numberOfInputVal; i++ {
-			(*W1)[i] = make([]float64, *numberOfInputVal)
-		}
-
-		for i := 0; i < *numberOfInputVal; i++ {
-			(*W2)[i] = make([]float64, *numberOfInputVal)
-		}
-
-		for i := 0; i < *numberOfInputVal; i++ {
-			Print("Input_", i, " ")
-			_, err = Scan(&(*Input)[i])
-			if err != nil {
-				Println(err)
-				i -= 1
-			}
-		}
-
-		Println()
-
-		for i := 0; i < *numberOfInputVal; i++ {
-			for j := 0; j < *numberOfInputVal; j++ {
-				Print("Weight matrix W1_", i, "", j, " ")
-				_, err = Scan(&(*W1)[i][j])
-				if err != nil {
-					Println(err)
-					j -= 1
-				}
-			}
-		}
-
-		Println()
-
-		for i := 0; i < *numberOfInputVal; i++ {
-			Print("Bias B2_", i, " ")
-			_, err = Scan(&(*B2)[i])
-			if err != nil {
-				Println(err)
-				i -= 1
-			}
-		}
-
-		Println()
-
-		for i := 0; i < *numberOfInputVal; i++ {
-			for j := 0; j < *numberOfInputVal; j++ {
-				Print("Weight matrix W2_", i, "", j, " ")
-				_, err = Scan(&(*W2)[i][j])
-				if err != nil {
-					Println(err)
-					j -= 1
-				}
-			}
-		}
-
-		Println()
-
-		for i := 0; i < *numberOfInputVal; i++ {
-			Print("Bias B3_", i, " ")
-			_, err = Scan(&(*B3)[i])
-			if err != nil {
-				Println(err)
-				i -= 1
-			}
-		}
-
-		Println()
-
-		for i := 0; i < *numberOfInputVal; i++ {
-			Print("Target values Y_", i, " ")
-			_, err = Scan(&(*Y)[i])
-			if err != nil {
-				Println(err)
-				i -= 1
-			}
-		}
-
-		Println()
-	}
-
-	return *Input, *B2, *B3, *Y, *W1, *W2
 }
